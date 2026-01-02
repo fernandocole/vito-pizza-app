688a8218d7126bf1bcac1a73dcfd7065cc4a207e
fernandocole <fernandocole@users.noreply.github.com>
2026-01-01 23:40:30 -0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -9,6 +9,7 @@ import {
 } from 'lucide-react';
 
 // Imports de Vistas
+/*
 import { KitchenView } from '@/app/components/admin/views/KitchenView';
 import { OrdersView } from '@/app/components/admin/views/OrdersView';
 import { InventoryView } from '@/app/components/admin/views/InventoryView';
@@ -17,7 +18,7 @@ import { RankingView } from '@/app/components/admin/views/RankingView';
 import { UsersView } from '@/app/components/admin/views/UsersView';
 import { ConfigView } from '@/app/components/admin/views/ConfigView';
 import { LogsView } from '@/app/components/admin/views/LogsView';
-
+*/
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
